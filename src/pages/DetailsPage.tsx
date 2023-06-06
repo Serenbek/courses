00f557b377fc37b295f88ad3b239098b151dbab1
Ani25ani <anjarsuper51@gmail.com>
2023-06-06 16:57:47 +0600

    feat/Complete-Digital

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,10 +1,12 @@
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
+import CompleteDigital from "../components/CompleteDigital/CompleteDigital";
 
 const DetailsPage = () => {
   return (
     <>
       <Header />
+      <CompleteDigital/>
       <Footer />
     </>
   );
