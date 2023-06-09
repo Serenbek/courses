a8e2519de70c601d6530c5dd5fcbb1787042b22f
MAXONCHIK236 <m-garzhaikin@bk.ru>
2023-06-09 12:05:17 +0300

    feat/listpage

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,13 +1,11 @@
 import Footer from "../components/footer/footer";
 import Header from "../components/header/header";
-import Lessons from './../components/Lessons/Lessons';
 import CompleteDigital from "../components/CompleteDigital/CompleteDigital";
 
 const DetailsPage = () => {
   return (
     <>
       <Header />
-      <Lessons/>
       <CompleteDigital/>
       <Footer />
     </>
